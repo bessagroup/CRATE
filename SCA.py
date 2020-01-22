539c472e7ac63cac3be1328cf27906308d43e325
Bernardo Ferreira <bpferreira@fe.up.pt>
2020-01-22 10:22:21 +0000

    Start SCA implementation

---
+++ SCA.py
@@ -0,0 +1,33 @@
+#
+# Self-Consistent Clustering Analysis (SCA) - Clustering Reduced Order Model
+# ==========================================================================================
+# Summary:
+# ...
+# ------------------------------------------------------------------------------------------
+# Description:
+# ...
+# ------------------------------------------------------------------------------------------
+# Author(s):
+# This program initial version was coded by Bernardo P. Ferreira (bpferreira@fe.up.pt,
+# CM2S research group, Department of Mechanical Engineering, Faculty of Engineering,
+# University of Porto) and developed in colaboration with Dr. Miguel A. Bessa
+# (m.a.bessa@tudelft.nl, Faculty of Mechanical, Maritime and Materials Engineering,
+# Delft University of Technology) and Dr. Francisco M. Andrade Pires (fpires@fe.up.pt,
+# CM2S research group, Department of Mechanical Engineering, Faculty of Engineering,
+# University of Porto).
+# ------------------------------------------------------------------------------------------
+# Credits:
+# This program structure was inspired on the original Self-Consistent Clustering Analysis
+# Matlab code implemented and developed by Dr. Zeliang Liu and Dr. Miguel A. Bessa in the
+# course of the research published in "Self-consistent clustering analysis: an efficient
+# multi-scale scheme for inelastic heterogeneous materials. Comp Methods Appl M 305 (2016):
+# 319-341 (Liu, Z., Bessa, M.A. and Liu, W.K.)".
+#
+# ------------------------------------------------------------------------------------------
+# Licensing and Copyrights:
+# ...
+# ------------------------------------------------------------------------------------------
+# Development history:
+# Z.Liu & M.A.Bessa    |     2016     | Original SCA Matlab code (see credits)
+# Bernardo P. Ferreira | January 2020 | Initial coding.
+# ==========================================================================================
