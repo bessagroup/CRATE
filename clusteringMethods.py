680aa43e8fe36de6cf32561cafd743858a5574a3
Bernardo Ferreira <bpferreira@fe.up.pt>
2020-02-12 09:55:36 +0000

    Finished clusteringMethods module

--- clusteringMethods.py
+++ clusteringMethods.py
@@ -22,7 +22,8 @@ import tensorOperations as top
 #
 #                                                                         Perform clustering
 # ==========================================================================================
-# ...
+# Perform clustering processes according to the selected clustering strategy by employing
+# the selected clustering method
 def performClustering(mat_dict,rg_dict,clst_dict):
     # Get material data
     n_material_phases = mat_dict['n_material_phases']
@@ -37,8 +38,12 @@ def performClustering(mat_dict,rg_dict,clst_dict):
     phase_nclusters = clst_dict['phase_nclusters']
     clst_dataidxs = clst_dict['clst_dataidxs']
     clst_quantities = clst_dict['clst_quantities']
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Initialize clustering processes labels list
     clst_processes = list()
+    # Initialize label correction (avoid that different material phases as well as different
+    # clustering processes share the same labels)
+    label_correction = 0
     # Perform clustering processes according to the selected clustering method
     if clustering_method == 1:
         info.displayInfo('5','Performing K-Means clustering...')
@@ -47,7 +52,6 @@ def performClustering(mat_dict,rg_dict,clst_dict):
             info.displayInfo('6','progress',iclst+1,len(clst_dataidxs))
             # Initialize current clustering process labels
             clst_process_lbls_flat = np.full(n_voxels,-1,dtype=int)
-            label_correction = 0
             # Get current clustering process data indexes
             data_indexes = clst_dataidxs[iclst]
             # Loop over material phases
@@ -63,15 +67,41 @@ def performClustering(mat_dict,rg_dict,clst_dict):
                 # Store current material phase cluster labels
                 clst_process_lbls_flat[phase_voxel_flatidx[mat_phase]] = \
                                                            kmeans.labels_ + label_correction
-                # Update label correction so that different material phases do not share the
-                # same label
+                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Validation
+                if False:
+                    # Set functions being validated
+                    val_functions = ['performClustering()',]
+                    # Display validation header
+                    print('\nValidation: ',
+                            (len(val_functions)*'{}, ').format(*val_functions), 3*'\b', ' ')
+                    print(92*'-')
+                    # Display validation
+                    np.set_printoptions(linewidth=np.inf)
+                    print('\nClustering process: ', iclst + 1)
+                    print('\nData indexes: ', data_indexes)
+                    print('\nMaterial phase: ', mat_phase)
+                    print('\nNumber of clusters: ', n_clusters)
+                    print('\nClustering quantities:')
+                    print(clst_quantities)
+                    print('\nMaterial phase indexes:',phase_voxel_flatidx[mat_phase])
+                    print('\nMaterial phase dataset:')
+                    print(dataset)
+                    print('\nLabels (-1 means not labeled):')
+                    print(clst_process_lbls_flat)
+                    # Display validation footer
+                    print('\n' + 92*'-' + '\n')
+                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Update label correction
                 label_correction = label_correction + n_clusters
             # Store current clustering process labels list
             clst_processes.append(list(clst_process_lbls_flat))
         info.displayInfo('6','completed')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Perform RVE clustering discretization according to the selected clustering strategy
     if clustering_strategy == 1:
         # Get the cluster labels from the unique clustering process
         clst_dict['voxels_clstlbl_flat'] = clst_processes[0]
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Return
     return None
