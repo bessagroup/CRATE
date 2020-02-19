Validated CIT Computation + ... - Added material phase to the clustering VTK file - Fixed isinstance(x,int) in VTK module (this should be done in all modules) - Created .cit file

SCA.py
@@ -59,6 +59,8 @@ import packager
 import clusteringQuantities
 # Perform clustering
 import clusteringMethods
+# Cluster interaction tensors computation
+import clusterInteractionTensors
 # VTK output
 import VTKOutput
 #
@@ -76,12 +78,12 @@ input_file_name,input_file_path,input_file_dir = \
                                        fileOperations.setInputDataFilePath(str(sys.argv[1]))
 # Set problem name, directory and main subdirectories
 problem_name,problem_dir,offline_stage_dir,postprocess_dir,is_same_offstage,\
-cluster_file_path,hres_file_path \
+cluster_file_path,cit_file_path,hres_file_path \
                              = fileOperations.setProblemDirs(input_file_name,input_file_dir)
 # Package data associated to directories and paths
 dirs_dict = packager.packageDirsPaths(input_file_name,input_file_path,input_file_dir,
                                  problem_name,problem_dir,offline_stage_dir,postprocess_dir,
-                                 cluster_file_path,hres_file_path)
+                                 cluster_file_path,cit_file_path,hres_file_path)
 # Open user input data file
 try:
     input_file = open(input_file_path,'r')
@@ -148,7 +150,8 @@ vtk_dict = packager.packageVTK()
 phase_end_time = time.time()
 phase_names.append('Read input data')
 phase_times = np.append(phase_times,[[phase_init_time,phase_end_time]],axis=0)
-info.displayInfo('3','Read input data file',phase_times[1,1]-phase_times[1,0])
+info.displayInfo('3','Read input data file',
+                phase_times[phase_times.shape[0]-1,1]-phase_times[phase_times.shape[0]-1,0])
 #
 #                                      Offline stage: Compute clustering-defining quantities
 # ==========================================================================================
@@ -163,8 +166,8 @@ clusteringQuantities.computeClusteringQuantities(copy.deepcopy(problem_dict),
 phase_end_time = time.time()
 phase_names.append('Compute cluster-defining quantities')
 phase_times = np.append(phase_times,[[phase_init_time,phase_end_time]],axis=0)
-info.displayInfo('3','Compute cluster-defining quantities', \
-                                                          phase_times[1,1]-phase_times[1,0])
+info.displayInfo('3','Compute cluster-defining quantities',
+                phase_times[phase_times.shape[0]-1,1]-phase_times[phase_times.shape[0]-1,0])
 #
 #                                                          Offline stage: Perform clustering
 # ==========================================================================================
@@ -175,12 +178,29 @@ phase_init_time = time.time()
 clusteringMethods.performClustering(copy.deepcopy(dirs_dict),copy.deepcopy(mat_dict),
                                                            copy.deepcopy(rg_dict),clst_dict)
 # Write clustering VTK file
-VTKOutput.writeVTKClusterFile(vtk_dict,dirs_dict,rg_dict,clst_dict)
+VTKOutput.writeVTKClusterFile(vtk_dict,copy.deepcopy(dirs_dict),copy.deepcopy(rg_dict),
+                                                                   copy.deepcopy(clst_dict))
 # Set phase ending time and display finishing phase information
 phase_end_time = time.time()
 phase_names.append('Perform clustering')
 phase_times = np.append(phase_times,[[phase_init_time,phase_end_time]],axis=0)
-info.displayInfo('3','Perform clustering',phase_times[1,1]-phase_times[1,0])
+info.displayInfo('3','Perform clustering',
+                phase_times[phase_times.shape[0]-1,1]-phase_times[phase_times.shape[0]-1,0])
+#
+#                                         Offline stage: Compute cluster interaction tensors
+# ==========================================================================================
+# Display starting phase information and set phase initial time
+info.displayInfo('2','Compute cluster interaction tensors')
+phase_init_time = time.time()
+# Compute the cluster interaction tensors
+clusterInteractionTensors.computeClusterInteractionTensors(copy.deepcopy(dirs_dict),
+                               copy.deepcopy(problem_dict),copy.deepcopy(rg_dict),clst_dict)
+# Set phase ending time and display finishing phase information
+phase_end_time = time.time()
+phase_names.append('Compute cluster interaction tensors')
+phase_times = np.append(phase_times,[[phase_init_time,phase_end_time]],axis=0)
+info.displayInfo('3','Compute cluster interaction tensors',
+                phase_times[phase_times.shape[0]-1,1]-phase_times[phase_times.shape[0]-1,0])
 #                                                                                End program
 # ==========================================================================================
 # Get current time and date

VTKOutput.py
@@ -17,6 +17,8 @@ import sys
 import numpy as np
 # Shallow and deep copies
 import copy
+# Display messages
+import info
 #
 #                                                                           Global variables
 # ==========================================================================================
@@ -27,10 +29,13 @@ indent = '  '
 # ==========================================================================================
 # Write VTK file with the clustering discretization
 def writeVTKClusterFile(vtk_dict,dirs_dict,rg_dict,clst_dict):
+    info.displayInfo('5','Writing cluster VTK file...')
     # Get input data file name
     input_file_name = dirs_dict['input_file_name']
     # Get offline stage directory
     offline_stage_dir = dirs_dict['offline_stage_dir']
+    # Get the spatial discretization file (regular grid of pixels/voxels)
+    regular_grid = rg_dict['regular_grid']
     # Get number of pixels/voxels in each dimension and total number of pixels/voxels
     n_voxels_dims = rg_dict['n_voxels_dims']
     # Get RVE dimensions
@@ -62,14 +67,25 @@ def writeVTKClusterFile(vtk_dict,dirs_dict,rg_dict,clst_dict):
     # Open VTK dataset element piece
     writeVTKOpenPiece(vtk_file,piece_parameters)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Write VTK cell data array
+    # Open VTK dataset element piece cell data
+    writeVTKOpenCellData(vtk_file)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Write VTK cell data array - Material phases
+    data_list = list(regular_grid.flatten())
+    min_val = min(data_list)
+    max_val = max(data_list)
+    data_parameters = {'Name':'Material phase','format':'ascii','RangeMin':min_val,
+                                                                         'RangeMax':max_val}
+    writeVTKCellDataArray(vtk_file,vtk_dict,data_list,data_parameters)
+    # Write VTK cell data array - Clusters
     data_list = list(voxels_clusters.flatten())
     min_val = min(data_list)
     max_val = max(data_list)
     data_parameters = {'Name':'Cluster','format':'ascii','RangeMin':min_val,
                                                                          'RangeMax':max_val}
-    writeVTKOpenCellData(vtk_file)
     writeVTKCellDataArray(vtk_file,vtk_dict,data_list,data_parameters)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Close VTK dataset element piece cell data
     writeVTKCloseCellData(vtk_file)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Close VTK dataset element piece
@@ -275,7 +291,7 @@ def writeVTKCloseCellData(vtk_file):
 # Write VTK cell data array
 def writeVTKCellDataArray(vtk_file,vtk_dict,data_list,data_parameters):
     # Set cell data array data type and associated ascii format
-    if all('int' in str(type(x)).lower() for x in data_list):
+    if all(isinstance(x,int) or isinstance(x,np.integer) for x in data_list):
         data_type = 'Int32'
         frmt = 'd'
     elif all('bool' in str(type(x)).lower() for x in data_list):

clusteringMethods.py
@@ -19,12 +19,18 @@ import sklearn.cluster
 import pickle
 # Inspect file name and line
 import inspect
+# Generate efficient iterators
+import itertools as it
+# Shallow and deep copy operations
+import copy
 # Display messages
 import info
 # Display errors, warnings and built-in exceptions
 import errors
 # Tensorial operations
 import tensorOperations as top
+# Imported for validation against Matlab only (remove)
+import scipy.io as sio
 #
 #                                                                         Perform clustering
 # ==========================================================================================
@@ -113,15 +119,64 @@ def performClustering(dirs_dict,mat_dict,rg_dict,clst_dict):
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Perform RVE clustering discretization according to the selected clustering strategy
     if clustering_strategy == 1:
-        # Store the cluster labels from the unique clustering process (regular grid shape)
-        clst_dict['voxels_clusters'] = \
-                                np.array(clst_processes[0],dtype=int).reshape(n_voxels_dims)
-        voxels_clusters = clst_dict['voxels_clusters']
+        # Build cluster labels from the unique clustering process (regular grid shape)
+        voxels_clusters = np.array(clst_processes[0],dtype=int).reshape(n_voxels_dims)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Initialize mapping dictionary to sort the cluster labels in asceding order of material
+    # phase
+    sort_dict = dict()
+    # Initialize material phase initial cluster label
+    lbl_init = 0
+    # Loop over material phases sorted in ascending order
+    sorted_mat_phases = list(np.sort(list(phase_nclusters.keys())))
+    for mat_phase in sorted_mat_phases:
+        # Get old cluster labels
+        old_clusters = np.unique(voxels_clusters.flatten()[phase_voxel_flatidx[mat_phase]])
+        # Set new cluster labels
+        new_clusters = list(range(lbl_init,lbl_init+phase_nclusters[mat_phase]))
+        # Set next material phase initial cluster label
+        lbl_init = lbl_init + phase_nclusters[mat_phase]
+        # Build mapping dictionary to sort the cluster labels
+        for i in range(phase_nclusters[mat_phase]):
+            if old_clusters[i] in sort_dict.keys():
+                location = inspect.getframeinfo(inspect.currentframe())
+                errors.displayError('E00038',location.filename,location.lineno+1)
+            else:
+                sort_dict[old_clusters[i]] = new_clusters[i]
+    # Check mapping dictionary
+    if np.any(np.sort(list(sort_dict.keys())) != range(sum(phase_nclusters.values()))):
+        location = inspect.getframeinfo(inspect.currentframe())
+        errors.displayError('E00039',location.filename,location.lineno+1)
+    elif np.any(np.sort([sort_dict[key] for key in sort_dict.keys()]) != \
+                                                      range(sum(phase_nclusters.values()))):
+        location = inspect.getframeinfo(inspect.currentframe())
+        errors.displayError('E00039',location.filename,location.lineno+1)
+    # Sort cluster labels in ascending order of material phase
+    for voxel_idx in it.product(*[list(range(n_voxels_dims[i])) \
+                                                       for i in range(len(n_voxels_dims))]):
+        voxels_clusters[voxel_idx] = sort_dict[voxels_clusters[voxel_idx]]
+    # Store cluster labels
+    clst_dict['voxels_clusters'] = voxels_clusters
+    # --------------------------------------------------------------------------------------
+    # Validation:
+    if False:
+        # Dump voxels_clusters (Matlab consistent rowise flat format) in Matlab file
+        print('\nidx_from_python:')
+        print(voxels_clusters.flatten('F'))
+        matlab_dic = {'idx_from_python':voxels_clusters.flatten('F')}
+        sio.savemat(dirs_dict['input_file_dir'] + '/' + dirs_dict['input_file_name'] +
+                                                                          '_idx',matlab_dic)
+    # --------------------------------------------------------------------------------------
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Store material clusters belonging to each material phase
-    for mat_phase in phase_voxel_flatidx.keys():
-        clst_dict['phase_clusters'] = \
+    for mat_phase in phase_nclusters.keys():
+        clst_dict['phase_clusters'][mat_phase] = \
                         np.unique(voxels_clusters.flatten()[phase_voxel_flatidx[mat_phase]])
+    # --------------------------------------------------------------------------------------
+    # Validation:
+    if False:
+        print('\nCheck cluster labels sorting:\n', clst_dict['phase_clusters'])
+    # --------------------------------------------------------------------------------------
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Compute voxel volume
     voxel_vol = np.prod([float(rve_dims[i])/n_voxels_dims[i] for i in range(len(rve_dims))])
@@ -131,6 +186,13 @@ def performClustering(dirs_dict,mat_dict,rg_dict,clst_dict):
     for cluster in np.unique(voxels_clusters):
         clst_dict['clusters_f'][str(cluster)] = \
                                       (np.sum(voxels_clusters == cluster)*voxel_vol)/rve_vol
+        # ----------------------------------------------------------------------------------
+        # Validation:
+        if False:
+            print('\nCluster volume fractions:')
+            print('\ncluster:', cluster, ' volume fraction:', \
+                                                      clst_dict['clusters_f'][str(cluster)])
+        # ----------------------------------------------------------------------------------
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Open file which contains all the required information associated to the clustering
     # discretization
@@ -139,7 +201,8 @@ def performClustering(dirs_dict,mat_dict,rg_dict,clst_dict):
     except Exception as message:
         location = inspect.getframeinfo(inspect.currentframe())
         errors.displayException(location.filename,location.lineno+1,message)
-    # Store clustering data
+    # Dump clustering data
+    info.displayInfo('5','Storing clustering file (.clusters)...')
     pickle.dump(clst_dict,cluster_file)
     # Close file
     cluster_file.close()

clusteringQuantities.py
@@ -68,10 +68,11 @@ def computeClusteringQuantities(problem_dict,mat_dict,rg_dict,clst_dict):
     problem_type = problem_dict['problem_type']
     n_dim = problem_dict['n_dim']
     comp_order_sym = problem_dict['comp_order_sym']
-    # Extract the required data from the spatial discretization file(s) according to the
-    # chosen solution method to compute the cluster-defining quantities
-    info.displayInfo('5','Reading discretization file...')
+    # Get clustering data
     clustering_solution_method = clst_dict['clustering_solution_method']
+    clustering_strategy = clst_dict['clustering_strategy']
+    # Get the required data from the spatial discretization file(s) according to the
+    # chosen solution method to compute the cluster-defining quantities
     if clustering_solution_method == 1:
         # Get the spatial discretization file (regular grid of pixels/voxels)
         regular_grid = rg_dict['regular_grid']
@@ -80,10 +81,9 @@ def computeClusteringQuantities(problem_dict,mat_dict,rg_dict,clst_dict):
         n_voxels = np.prod(n_voxels_dims)
         # Get RVE dimensions
         rve_dims = rg_dict['rve_dims']
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Compute the required cluster-defining quantities according to the adopted clustering
     # strategy and set clustering processes quantities list
-    info.displayInfo('5','Computing clustering-defining quantities:')
-    clustering_strategy = clst_dict['clustering_strategy']
     if clustering_strategy == 1:
         # In this clustering strategy, only one clustering process is performed based on the
         # strain concentration fourth-order tensor. Initialize the clustering quantities
@@ -94,7 +94,7 @@ def computeClusteringQuantities(problem_dict,mat_dict,rg_dict,clst_dict):
         clst_dataidxs = [list(range(n_clustering_var)),]
         # Small strain formulation
         if strain_formulation == 1:
-            info.displayInfo('5','Computing strain concentration tensors...',2)
+            info.displayInfo('5','Computing strain concentration tensors...')
             # Loop over independent strain components
             for i in range(len(comp_order_sym)):
                 compi = comp_order_sym[i]

packager.py
@@ -15,12 +15,15 @@ import numpy as np
 import ntpath
 # Generate efficient iterators
 import itertools as it
+# Display messages
+import info
 #
 #                                                                          Package functions
 # ==========================================================================================
 # Package directories and paths
 def packageDirsPaths(input_file_name,input_file_path,input_file_dir,problem_name,
-            problem_dir,offline_stage_dir,postprocess_dir,cluster_file_path,hres_file_path):
+              problem_dir,offline_stage_dir,postprocess_dir,cluster_file_path,cit_file_path,
+                                                                            hres_file_path):
     # Initialize directories and paths dictionary
     dirs_dict = dict()
     # Build directories and paths dictionary
@@ -31,6 +34,7 @@ def packageDirsPaths(input_file_name,input_file_path,input_file_dir,problem_name
     dirs_dict['problem_dir'] = problem_dir
     dirs_dict['offline_stage_dir'] = offline_stage_dir
     dirs_dict['cluster_file_path'] = cluster_file_path
+    dirs_dict['cit_file_path'] = cit_file_path
     dirs_dict['hres_file_path'] = hres_file_path
     dirs_dict['postprocess_dir'] = postprocess_dir
     # Return
@@ -78,6 +82,7 @@ def packageRegularGrid(discret_file_path,rve_dims,mat_dict,problem_dict):
     n_material_phases = mat_dict['n_material_phases']
     material_properties = mat_dict['material_properties']
     # Read the spatial discretization file (regular grid of pixels/voxels)
+    info.displayInfo('5','Reading discretization file...')
     if ntpath.splitext(ntpath.basename(discret_file_path))[-1] == '.npy':
         regular_grid = np.load(discret_file_path)
     else: