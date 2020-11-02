2b99633fd79b2dc48355d2b652f2575e050c1a91
Bernardo Ferreira <bpferreira@fe.up.pt>
2020-11-02 19:10:30 +0000

    Updated documentation of Homogenization module.

--- online/homogenization/homogenization.py
+++ online/homogenization/homogenization.py
@@ -2,11 +2,12 @@
 # Homogenization Module (CRATE Program)
 # ==========================================================================================
 # Summary:
-# Procedures related to the computation of the homogenized strain and stress fields as well
-# as the computation of the effective tangent modulus.
+# Procedures related to the computation of the homogenized strain and stress tensors and to
+# the computation of the CRVE effective tangent modulus.
 # ------------------------------------------------------------------------------------------
 # Development history:
-# Bernardo P. Ferreira | February 2020 | Initial coding.
+# Bernardo P. Ferreira | Feb 2020 | Initial coding.
+# Bernardo P. Ferreira | Nov 2020 | Updated documentation.
 # ==========================================================================================
 #                                                                             Import modules
 # ==========================================================================================
@@ -20,13 +21,41 @@ import tensor.tensoroperations as top
 # Matricial operations
 import tensor.matrixoperations as mop
 #
-#                                                          Strain/Stress homogenized tensors
+#                                                          Homogenized strain/stress tensors
 # ==========================================================================================
-# Compute homogenized strain and stress tensors (matricial form)
-def homstatetensors(problem_dict, material_phases, phase_clusters, clusters_f,
+def homstatetensors(comp_order, material_phases, phase_clusters, clusters_f,
                     clusters_state):
-    # Get problem data
-    comp_order = problem_dict['comp_order_sym']
+    '''Compute the homogenized strain and stress tensors.
+
+    Parameters
+    ----------
+    comp_order : list
+        Strain/Stress components (str) order.
+    material_phases : list
+        RVE material phases labels (str).
+    phase_clusters : dict
+        Clusters labels (item, list of int) associated to each material phase (key, str).
+    clusters_f : dict
+        Clusters volume fraction (item, float) associated to each material cluster
+        (key, str).
+    clusters_state : dict
+        Material constitutive model state variables (item, dict) associated to each material
+        cluster (key, str). Despite the several constitutive model dependent state
+        variables, the strain and stress tensors matricial form must be available through
+        the keys 'strain_mf' and 'stress_mf', respectively.
+
+    Returns
+    -------
+    hom_strain_mf : ndarray of shape (n_comps,)
+        Homogenized strain tensor in matricial form.
+    hom_stress_mf : ndarray of shape (n_comps,)
+        Homogenized stress tensor in matricial form.
+
+    Notes
+    -----
+    The matricial form storage is perform according to the provided strain/stress
+    components order.
+    '''
     # Initialize incremental homogenized strain and stress tensors (matricial form)
     hom_strain_mf = np.zeros(len(comp_order))
     hom_stress_mf = np.zeros(len(comp_order))
@@ -41,19 +70,45 @@ def homstatetensors(problem_dict, material_phases, phase_clusters, clusters_f,
             # (matricial form)
             hom_strain_mf = np.add(hom_strain_mf, clusters_f[str(cluster)]*strain_mf)
             hom_stress_mf = np.add(hom_stress_mf, clusters_f[str(cluster)]*stress_mf)
-    # Return
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     return [hom_strain_mf, hom_stress_mf]
 # ------------------------------------------------------------------------------------------
-# Compute homogenized out-of-plane strain or stress component in 2D plane strain and plane
-# stress problems (output purpose only)
 def homoutofplanecomp(problem_type, material_phases, phase_clusters, clusters_f,
-                      clusters_state, clusters_state_old):
+                      clusters_state):
+    '''Compute homogenized out-of-plane strain or stress component.
+
+    Parameters
+    ----------
+    problem_type : {1, 2}
+        Plane problem type identifier: (1) Plane strain, (2) Plane stress. This parameter
+        determines the nature of the out-of-plain homogenized component that is computed.
+    material_phases : list
+        RVE material phases labels (str).
+    phase_clusters : dict
+        Clusters labels (item, list of int) associated to each material phase (key, str).
+    clusters_f : dict
+        Clusters volume fraction (item, float) associated to each material cluster
+        (key, str).
+    clusters_state : dict
+        Material constitutive model state variables (item, dict) associated to each material
+        cluster (key, str). Despite the several constitutive model dependent state
+        variables, the non-null out-of-plain strain or stress component must be available
+        through the keys 'strain_33' or 'stress_33', respectively.
+
+    Return
+    ------
+    hom_comp : float
+        Homogenized out-of-plane strain or stress component.
+    '''
     # Set out-of-plane stress component (2D plane strain problem) / strain component
     # (2D plane stress problem)
     if problem_type == 1:
         comp_name = 'stress_33'
     elif problem_type == 2:
         comp_name = 'strain_33'
+    else:
+        raise RuntimeError('Unknown plane problem type.')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Initialize homogenized out-of-plane component
     hom_comp = 0.0
     # Loop over material phases
@@ -64,20 +119,52 @@ def homoutofplanecomp(problem_type, material_phases, phase_clusters, clusters_f,
             # component
             hom_comp = hom_comp + \
                 clusters_f[str(cluster)]*clusters_state[str(cluster)][comp_name]
-    # Return
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     return hom_comp
 #
-#                                                                  Effective tangent modulus
+#                                                             CRVE effective tangent modulus
 # ==========================================================================================
 # Compute effective tangent modulus
-def efftanmod(problem_dict, material_phases, n_total_clusters, phase_clusters, clusters_f,
-              clusters_D_mf, global_cit_D_De_ref_mf):
-    # Get problem data
-    n_dim = problem_dict['n_dim']
-    comp_order = problem_dict['comp_order_sym']
+def efftanmod(n_dim, comp_order, material_phases, phase_clusters, clusters_f, clusters_D_mf,
+              global_cit_D_De_ref_mf):
+    '''Compute CRVE effective (homogenized) tangent modulus.
+
+    Parameters
+    ----------
+    n_dim : int
+        Problem dimension.
+    comp_order : list
+        Strain/Stress components (str) order.
+    phase_clusters : dict
+        Clusters labels (item, list of int) associated to each material phase (key, str).
+    clusters_f : dict
+        Clusters volume fraction (item, float) associated to each material cluster
+        (key, str).
+    clusters_D_mf : dict
+        Consistent tangent modulus (item, ndarray of shape(n_comps, n_comps)) associated to
+        each material cluster (key, str).
+    global_cit_D_De_ref_mf : ndarray
+        Global matrix with the same shape of the global cluster interaction matrix but where
+        each cluster interaction tensor (I)(J) is double contracted with the difference
+        between the material cluster (J) consistent tangent modulus and the reference
+        material elastic tangent modulus (assembled in matricial form).
+
+    Returns
+    -------
+    eff_tangent_mf : ndarray of shape(n_comps, n_comps)
+        CRVE effective (homogenized) tangent modulus in matricial form.
+
+    Notes
+    -----
+    The matricial form storage is perform according to the provided strain/stress
+    components order.
+    '''
     # Set second-order identity tensor
     _, _, _, fosym, _, _, _ = top.getidoperators(n_dim)
     FOSym_mf = mop.gettensormf(fosym, n_dim, comp_order)
+    # Get total number of clusters
+    n_total_clusters = len(clusters_f.keys())
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Compute cluster strain concentration tensors system of linear equations coefficient
     # matrix (derivatives of clusters equilibrium residuals)
     csct_matrix = np.add(scipy.linalg.block_diag(*(n_total_clusters*[FOSym_mf,])),
@@ -90,6 +177,7 @@ def efftanmod(problem_dict, material_phases, n_total_clusters, phase_clusters, c
     # Solve cluster strain concentration tensors system of linear equations
     for i in range(len(comp_order)):
         csct_solution[:, i] = numpy.linalg.solve(csct_matrix, csct_rhs[:, i])
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Initialize effective tangent modulus
     eff_tangent_mf = np.zeros((len(comp_order), len(comp_order)))
     # Initialize cluster index
@@ -111,5 +199,5 @@ def efftanmod(problem_dict, material_phases, n_total_clusters, phase_clusters, c
             # Increment cluster index
             i_init = i_init + len(comp_order)
             i_end = i_init + len(comp_order)
-    # Return
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     return eff_tangent_mf
