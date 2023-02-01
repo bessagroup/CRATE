0224435cbacd74e70d2f2ea5552eeba8fde48cba
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2023-02-01 10:39:02 -0500

    Enforcing BRG coding style.

--- ioput/fileoperations.py
+++ ioput/fileoperations.py
@@ -1,36 +1,52 @@
+"""File and directory operations.
+
+This module...
+
+Functions
+---------
+makedirectory
+    Create new directory.
+rmunreqdirs
+    Remove unrequired directories and files in target directory.
+setinputdatafilepath
+    Process input data file path.
+setproblemdirs
+    Set output directory structure and output files paths.
+"""
 #
-# File Operations Module (CRATE Program)
-# ==========================================================================================
-# Summary:
-# Procedures related to operations on files, directories and associated paths. Among these
-# operations is the definition of the results folder associated to a given problem input
-# data file, directory where all the related output is stored.
-# ------------------------------------------------------------------------------------------
-# Development history:
-# Bernardo P. Ferreira | Jan 2020 | Initial coding.
-# Bernardo P. Ferreira | Oct 2020 | Replaced '.clusters' and '.cit' files by '.crve' file.
-# ==========================================================================================
-#                                                                             Import modules
-# ==========================================================================================
-# Operating system related functions
+#                                                                       Modules
+# =============================================================================
+# Standard
 import os
-# Operations on files and directories
 import shutil
-# Inspect file name and line
 import inspect
-# Extract information from path
 import ntpath
-# Display messages
+# Local
 import ioput.info as info
-# Display errors, warnings and built-in exceptions
 import ioput.errors as errors
-# I/O utilities
 import ioput.ioutilities as ioutil
 #
-#                                                                       Directory operations
-# ==========================================================================================
-# Make directory (both overwrite and no overwrite (default) options available)
+#                                                          Authorship & Credits
+# =============================================================================
+__author__ = 'Bernardo Ferreira (bernardo_ferreira@brown.edu)'
+__credits__ = ['Bernardo Ferreira',]
+__status__ = 'Stable'
+# =============================================================================
+#
+# =============================================================================
+#
+#                                                          Directory operations
+# =============================================================================
 def makedirectory(dir, option='no_overwrite'):
+    """Create new directory.
+
+    Parameters
+    ----------
+    dir : str
+        Path of new directory.
+    option : {'no_overwrite', 'overwrite'}, default='no_overwrite'
+        Either to overwrite or not an existing path.
+    """
     try:
         if option == 'no_overwrite':
             os.mkdir(dir)
@@ -40,11 +56,24 @@ def makedirectory(dir, option='no_overwrite'):
             else:
                 shutil.rmtree(dir)
                 os.mkdir(dir)
+        else:
+            raise RuntimeError('Unknown option.')
     except OSError as message:
         location = inspect.getframeinfo(inspect.currentframe())
-        errors.displayexception(location.filename, location.lineno + 1, message)
+        errors.displayexception(location.filename, location.lineno + 1,
+                                message)
+# =============================================================================
 # Remove all the unrequired directories and files in a target directory
 def rmunreqdirs(target_dir, required_dirnames):
+    """Remove unrequired directories and files in target directory.
+
+    Parameters
+    ----------
+    target_dir : str
+        Path of target directory.
+    required_dirnames : list[str]
+        Paths that are to be preserved in target directory.
+    """
     dirnames = os.listdir(target_dir)
     for dir in dirnames:
         if dir not in required_dirnames:
@@ -54,10 +83,25 @@ def rmunreqdirs(target_dir, required_dirnames):
             else:
                 shutil.rmtree(path)
 #
-#                                                    User input data file and problem folder
-# ==========================================================================================
-# Set input data file name, absolute path and directory
+#                                                               Input data file
+# =============================================================================
 def setinputdatafilepath(path):
+    """Process input data file path.
+
+    Parameters
+    ----------
+    path : str
+        Path of input data file.
+
+    Returns
+    -------
+    input_file_name : str
+        Input data file name.
+    input_file_path : str
+        Input data file path.
+    input_file_dir : str
+        Input data file directory path.
+    """
     # Set input data file path, directory, name and extension
     input_file_path = os.path.abspath(path)
     input_file_dir = ntpath.dirname(input_file_path) + '/'
@@ -67,124 +111,203 @@ def setinputdatafilepath(path):
     if input_file_ext != '.dat':
         location = inspect.getframeinfo(inspect.currentframe())
         errors.displayerror('E00010', location.filename, location.lineno + 1)
-    # Check if the input data file name only contains numbers, letters or underscores
+    # Check if the input data file name only contains numbers, letters or
+    # underscores
     if not ioutil.checkvalidname(input_file_name):
         location = inspect.getframeinfo(inspect.currentframe())
         errors.displayerror('E00002', location.filename, location.lineno + 1)
     # Return
-    return [input_file_name, input_file_path, input_file_dir]
-# ------------------------------------------------------------------------------------------
-# Set the problem folder (directories and files) as described below:
+    return input_file_name, input_file_path, input_file_dir
 #
-# example.dat
-# shinyregulargrid.rgmsh
-# example/ ------- example.screen
-#            |---- shinyregulargrid.rgmsh
-#            |---- Offline_Stage/ ------- example.crve
-#            |                      |---- example_clusters.vti
-#            |---- example.hres
-#            |---- example.refm
-#            |---- example.adapt
-#            |---- Post_Process/  ------- example.pvd
-#                                   |---- VTK/
-#                                   |---- example.voxout
-#
-# Folders and files meaning:
-# example.dat            - User input data file
-# shinyregulargrid.rgmsh - Example of spatial discretization file (doesn't need to be in the
-#                          same directory as the user input data file)
-# example/               - Problem folder
-# example.screen         - File where all the data printed to the default standard output
-#                          device is stored
-# shinyregulargrid.rgmsh - Copy of the spatial discretization file contained in the problem
-#                          folder
-# Offline_Stage/         - Offline stage folder
-# example.crve           - File which contains an offline state CRVE instance (binary
-#                          format)
-# example_clusters.vti   - VTK XML file with data related to the material phases and
-#                          material clusters
-# example.hres           - File where the homogenized results are stored
-# example.refm           - File where the reference material properties and associated
-#                          farfield strain are stored
-# example.adapt          - File where the adaptive material phases number of clusters and
-#                          adaptivity steps are stored (only if clustering adaptivity is
-#                          activated)
-# Post_Process/          - Post processing folder
-# example.pvd            - VTK XML file which contains the references to all the snapshots
-#                          of the problem solution
-# VTK/                   - Folder which constains all the snapshots of the problem solution
-#                          (VTK XML format)
-# example.voxout         - File where voxels material-related quantities are stored
-#
+#                                                             Results directory
+# =============================================================================
 def setproblemdirs(input_file_name, input_file_dir):
-    # Set problem name, directory
+    """Set output directory structure and output files paths.
+
+    *Output directory structure:*
+
+    .. code-block :: text
+
+       example.dat
+       microstructure_regular_grid.rgmsh
+       example/
+            |-----example.screen
+            |---- microstructure_regular_grid.rgmsh
+            |---- example.hres
+            |---- example.refm
+            |---- example.adapt
+            |---- Offline_Stage/
+            |           |---- example.crve
+            |           |---- example_clusters.vti
+            |
+            |---- Post_Process/
+                        |---- example.pvd
+                        |---- VTK/
+                        |---- example.voxout
+
+
+    *Glossary: Files and directories*
+
+    * **example.dat**
+        Input data file.
+
+    * **regular_grid_mesh.rgmsh**
+        Spatial discretization file (regular grid of voxels, where each voxel \
+        is associated with a given material phase ID).
+
+    * **example/**
+        Output directory.
+
+        * **example.screen**
+            File where all the data printed to the default standard output \
+            device is stored.
+
+        * **regular_grid_mesh.rgmsh**
+            Copy of spatial discretization file.
+
+        * **example.hres**
+            File where the homogenized strain/stress results are stored.
+
+        * **example.refm**
+            File where data associated with the homogeneous (fictitious)
+            reference material is stored.
+
+        * **example.adapt**
+            File where the adaptive material phases' number of clusters and \
+            adaptivity steps are stored (only if clustering adaptivity is \
+            activated).
+
+        * **Offline_Stage/**
+            Clustering-based model reduction (offline-stage) directory.
+
+            * **example.crve**
+                File that contains the CRVE instance obtained from the \
+                clustering-based model reduction (offline-stage), accounting \
+                for the base clustering and the cluster interaction tensors.
+
+            * **example_clusters.vti**
+                VTK XML file containing spatial (voxelwise) data \
+                characterizing the CRVE after the clustering-based model \
+                reduction (offline-stage), namely the material phases and the \
+                base material clusters.
+
+        * **Post_Process/**
+            Post-processing directory.
+
+            * **example.pvd**
+                VTK XML collection file containing the paths to all the VTK \
+                snapshots taken during the problem solution.
+
+            * **VTK/**
+                VTK XML files directory containing all the snapshots taken \
+                during the problem solution.
+
+            * **example.voxout**
+                File where material-related quantities defined at the voxel \
+                level are stored.
+
+    Parameters
+    ----------
+    input_file_name : str
+        Problem input data file name.
+    input_file_dir : str
+        Problem input data file directory path.
+
+    Returns
+    -------
+    problem_name : str
+        Problem name.
+    problem_dir : str
+        Problem output directory path.
+    offline_stage_dir : str
+        Problem output offline-stage subdirectory path.
+    postprocess_dir : str
+        Problem output post-processing subdirectory path.
+    is_same_offstage : bool
+        `True` if an already existing offline-stage data file is to be
+        considered, `False` otherwise.
+    crve_file_path : str
+        Problem '.crve' output file path.
+    hres_file_path : str
+        Problem '.hres' output file path.
+    refm_file_path : str
+        Problem '.refm' output file path.
+    adapt_file_path : str
+        Problem '.adapt' output file path.
+    """
+    # Set problem name and output directory
     problem_name = input_file_name
     problem_dir = input_file_dir + problem_name + '/'
-    # Set offline stage and post processing subdirectories
+    # Set offline-stage and post-processing subdirectories
     offline_stage_dir = problem_dir + 'Offline_Stage' + '/'
     postprocess_dir = problem_dir + 'Post_Process' + '/'
-    # Set '.screen' path and delete the file it it already exists
+    # Set '.screen' output file path (delete existing file)
     ioutil.screen_file_path = problem_dir + input_file_name + '.screen'
     if os.path.isfile(ioutil.screen_file_path):
         os.remove(ioutil.screen_file_path)
-    # Set '.crve' path
+    # Set '.crve' output file path
     crve_file_path = offline_stage_dir + input_file_name + '.crve'
-    # Set '.hres' path
+    # Set '.hres' output file path
     hres_file_path = problem_dir + input_file_name + '.hres'
-    # Set '.refm' path
+    # Set '.refm' output file path
     refm_file_path = problem_dir + input_file_name + '.refm'
-    # Set '.adapt' path
+    # Set '.adapt' output file path
     adapt_file_path = problem_dir + input_file_name + '.adapt'
-    # Check if the problem directory already exists or not
+    # Check if the problem output directory exists
     if not os.path.exists(problem_dir):
         status = 0
-        # Create problem directory and main subdirectories
+        # Create problem output directory and subdirectories
         makedirectory(problem_dir)
         for dir in [offline_stage_dir, postprocess_dir]:
             makedirectory(dir)
-        # There is no previously computed offline stage data
+        # No previously computed offline-stage data available
         is_same_offstage = False
     else:
-        ioutil.print2('\nWarning: The problem directory for the specified input data ' + \
-                      'file already exists.')
-        # Ask user if the purpose is to consider a previously computed offline state
-        # CRVE data file
-        is_same_offstage = ioutil.query_yn('\nDo you wish to consider the already ' + \
-                                           'existent offline state \'.crve\' datafile?',
-                                           'no')
+        ioutil.print2('\nWarning: The problem output directory for the '
+                      'specified input data file already exists.')
+        # Ask user if the purpose is to consider previously computed
+        # offline-stage data ('.crve' output file)
+        is_same_offstage = \
+            ioutil.query_yn('\nDo you wish to consider the already existent '
+                            'offline-stage \'.crve\' data file?', 'no')
         if is_same_offstage:
             status = 1
-            # If the offline stage subdirectory does not exist raise error
+            # Raise error if offline-stage subdirectory does not exist
             if not os.path.exists(offline_stage_dir):
                 location = inspect.getframeinfo(inspect.currentframe())
-                errors.displayerror('E00009', location.filename, location.lineno + 1,
-                                    problem_name, offline_stage_dir)
-            # Remove all the existent directories and files except the existent offline
-            # stage subdirectory and '.screen' file
+                errors.displayerror('E00009', location.filename,
+                                    location.lineno + 1, problem_name,
+                                    offline_stage_dir)
+            # Remove all the existent subdirectories and files except the
+            # offline-stage subdirectory and the '.screen' output file
             required_dirnames = [input_file_name + '.screen', 'Offline_Stage']
             rmunreqdirs(problem_dir, required_dirnames)
-            # Create post processing subdirectory
+            # Create post-processing subdirectory
             makedirectory(postprocess_dir, 'overwrite')
-            # Warn user to potential compatibility issues between the problem input data
-            # file and the already existent offline state '.crve' data file
-            ioutil.useraction('\n\nWarning: Please make sure that the problem input ' + \
-                              'data file is consistent with the already ' + '\n' + \
-                              len('Warning: ')*' ' + 'existent offline stage \'.crve\' ' + \
-                              'data file (stored in Offline_Stage/) to avoid ' + \
-                              '\n' + \
-                              len('Warning: ')*' ' + 'unexpected errors or ' + \
-                              'misleading conclusions.' + '\n\n' + \
-                              'Press any key to continue or type \'exit\' to leave: ')
+            # Warn user to potential compatibility issues between the problem
+            # input data file and the existent offline-stage '.crve' data file
+            ioutil.useraction('\n\nWarning: Please make sure that the problem '
+                              'input data file is consistent with the already'
+                              '\n' + len('Warning: ')*' '
+                              + 'existent offline-stage \'.crve\' data file '
+                              '(stored in Offline_Stage/) to avoid ' + '\n'
+                              + len('Warning: ')*' '
+                              + 'unexpected errors or misleading conclusions.'
+                              + '\n\n' + 'Press any key to continue or type '
+                              '\'exit\' to leave: ')
         else:
-            # Ask user if the existent problem directory is to be overwritten
-            is_overwrite = ioutil.query_yn('\nDo you wish to overwrite the problem ' + \
+            # Ask user if existent problem output directory should be
+            # overwritten
+            is_overwrite = ioutil.query_yn('\nDo you wish to overwrite the '
+                                           'existing problem output '
                                            'directory?')
             if is_overwrite:
                 status = 2
-                # Remove all the existent directories and files
+                # Remove all existent subdirectories and files except the
+                # '.screen' output file
                 required_dirnames = [input_file_name + '.screen']
                 rmunreqdirs(problem_dir, required_dirnames)
-                # Create problem directory and main subdirectories
+                # Create problem output directory subdirectories
                 for dir in [offline_stage_dir, postprocess_dir]:
                     makedirectory(dir, 'overwrite')
             else:
@@ -192,5 +315,6 @@ def setproblemdirs(input_file_name, input_file_dir):
         # Display information about the problem directory and status
         info.displayinfo('-1', problem_dir, status)
     # Return
-    return [problem_name, problem_dir, offline_stage_dir, postprocess_dir, is_same_offstage,
-            crve_file_path, hres_file_path, refm_file_path, adapt_file_path]
+    return problem_name, problem_dir, offline_stage_dir, postprocess_dir, \
+           is_same_offstage, crve_file_path, hres_file_path, refm_file_path, \
+           adapt_file_path
